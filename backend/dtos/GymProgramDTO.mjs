a0a215694570a7a71c756acc4e31b9342f37fc88
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-11-06 00:47:09 +0800

    Added GymProgramDTO.mjs

---
+++ backend/dtos/GymProgramDTO.mjs
@@ -0,0 +1,20 @@
+class GymProgramDTO
+{
+  /**
+   * @type { string }
+   * @public
+   */
+  name;
+
+  /**
+   * @type { boolean }
+   * @public
+   */
+  is_active;
+
+  constructor({ name, is_active })
+  {
+    this.name = name;
+    this.is_active = is_active;
+  }
+}
