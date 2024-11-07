dab7d4a3c1e00bf63cc41ff55b45117e53b207b5
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-11-07 21:05:20 +0800

    Implemented CORS ( Allow all origins )

--- backend/index.mjs
+++ backend/index.mjs
@@ -2,11 +2,20 @@ import express from "express";
 import bodyParser from "body-parser";
 import { MEMBERS_ROUTER } from "./routes/MemberRoutes.mjs";
 import { GYM_PROGRAMS_ROUTER } from "./routes/GymProgramRoutes.mjs";
+import cors from "cors";
 
 export let app = express();
 
 const PORT = process.env.PORT || 5050;
 const START_PAGE = "index.html";
+
+const CORS_OPTIONS =
+{
+    origin: true,
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(CORS_OPTIONS));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
