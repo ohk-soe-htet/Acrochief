5c353ef2df65acea7c130de021a636a8556a286d
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-11-09 05:00:46 +0800

    Initial layout for UpdateMember.mjs

---
+++ public/js/UpdateMember.mjs
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", onLoad);
+
+const onLoad = () =>
+{
+
+}
+
+// For modules ( .mjs ), we need to export the function
+window.updateMember = updateMember;
+
+const updateMember = () =>
+{
+
+}
