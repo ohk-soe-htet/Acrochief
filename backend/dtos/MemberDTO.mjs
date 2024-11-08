d079178656a7d9bf58baa20390abbf938699674d
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-11-08 22:12:20 +0800

    Initial work

--- backend/dtos/MemberDTO.mjs
+++ backend/dtos/MemberDTO.mjs
@@ -1,4 +1,36 @@
+import { z } from "zod";
 import { generateSnowflake } from "../helpers/SnowflakeHelpers.mjs";
+import { isAlpha } from "class-validator";
+
+export const MemberDTOSchema = z.object(
+{
+    name: z
+        .string()
+        .trim()
+        .min(1)
+        .max(50)
+        .refine(
+            name => name.split(' ').every(word => isAlpha(word)),
+            { message: "Name must only contain letters!" }
+        ),
+
+    adminNumber: z
+        .string()
+        .toUpperCase()
+        .regex(
+            /^\d{7}[A-Z]$/,
+            { message: "Invalid admin number format. It should consist of 7 digits followed by 1 uppercase letter (e.g., 2304806I)." }
+        ),
+
+    gymPrograms: z
+        .array(z.string())
+        .default([])
+        .transform(programs => new Set(programs))
+        .transform(programs => Array.from(programs))
+        .transform(programs => programs.map(program => program.toLowerCase()))
+});
+
+export const MemberUpdateDTOSchema = MemberDTOSchema.partial();
 
 export class MemberDTO
 {
