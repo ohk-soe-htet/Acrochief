bab7760f1c2763c02302460653ebdc031f78efe6
Ohk Soe Htet <ohksoe2002@gmail.com>
2025-02-10 03:36:47 +0800

    Completed the implementation of real-time monitoring and logging

--- index.mjs
+++ index.mjs
@@ -7,7 +7,15 @@ import { fileURLToPath } from "url";
 import path, { dirname } from "path";
 import cors from "cors";
 
+import statusMonitor from "express-status-monitor";
+import { logger } from "./logger.mjs";
+
+import client from "prom-client";
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
 export let app = express();
+app.use(statusMonitor());
 
 const PORT = process.env.PORT || 5050;
 const START_PAGE = "index.html";
@@ -27,6 +35,30 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "/dist")));
+
+const httpRequestDurationMicroseconds = new client.Histogram({
+	name: "http_request_duration_ms",
+	help: "Duration of HTTP requests in ms",
+	labelNames: ["method", "route", "code"],
+	buckets: [0.1, 5, 15, 50, 100, 300, 500, 1000, 3000, 5000], // buckets for response time from 0.1ms to 5s
+});
+
+app.use((req, res, next) => {
+	const end = httpRequestDurationMicroseconds.startTimer();
+	res.on("finish", () => {
+		end({
+			method: req.method,
+			route: req.route ? req.route.path : "",
+			code: res.statusCode,
+		});
+	});
+	next();
+});
+
+app.get("/metrics", async (req, res) => {
+	res.set("Content-Type", client.register.contentType);
+	res.end(await client.register.metrics());
+});
 
 const ROUTER = express.Router();
 
@@ -48,4 +80,6 @@ export let server = app.listen(PORT, function () {
 		address === "::" ? "localhost" : address
 	}:${port}`;
 	console.log(`Demo project at: ${baseUrl}`);
+	logger.info(`Demo project at: ${baseUrl}!`);
+	logger.error(`Example or error log`);
 });
